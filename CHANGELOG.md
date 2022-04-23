f523feea230e22b59228b572d04d8ed76215a3b3
maulvi <maulvi67@gmail.com>
2022-04-23 11:13:00 +0700

    Add convertion toStones

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,3 +2,5 @@
 
 All notable changes to `weight-conversion-package` will be documented in this file.
 
+## 1.0.0 - 2022-04-22
+- initial release
--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -1,5 +1,15 @@
 <?php
 
-class WeightTest
-{
-}
+use Maulvi67\WeightConversionPackage\WeightConversion;
+
+it('can convert kilograms to pounds correctly', function(){
+    $pounds = WeightConversion::kilograms(100)->toPounds();
+
+    expect($pounds)->toEqual(220.46);
+});
+
+it('can convert kilograms to stones correctly', function(){
+    $pounds = WeightConversion::kilograms(100)->toStones();
+
+    expect($pounds)->toEqual(15.747);
+});
