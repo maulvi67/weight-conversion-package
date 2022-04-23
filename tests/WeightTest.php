041c8b7f45bbfea1ae8c648501c5d4bdd43a8947
maulvi67 <maulvi67@users.noreply.github.com>
2022-04-23 04:13:42 +0000

    Fix styling

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -2,13 +2,13 @@
 
 use Maulvi67\WeightConversionPackage\WeightConversion;
 
-it('can convert kilograms to pounds correctly', function(){
+it('can convert kilograms to pounds correctly', function () {
     $pounds = WeightConversion::kilograms(100)->toPounds();
 
     expect($pounds)->toEqual(220.46);
 });
 
-it('can convert kilograms to stones correctly', function(){
+it('can convert kilograms to stones correctly', function () {
     $pounds = WeightConversion::kilograms(100)->toStones();
 
     expect($pounds)->toEqual(15.747);
